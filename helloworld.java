4df289fd315f8a1a3d1d81b07aadfedf3ac656f4
praveensomayaji <prvns01@gmail.com>
2021-10-10 11:37:17 +0000

    checking in the new helloworld file

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import java.io.*
+public class HelloWorld{
+String firstName = "praveen"
+String lastName = "Somayaji"
+int age = 30;
+
+
+public static void main(){
+	HelloWorld hi = new HelloWorld();
+	system.out.println("user details:"+hi.firstName +" "+hi.lastName+" "+hi.age);
+}
+}
+
